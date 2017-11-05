6f17ccab0bbd98c1bd757e3e492ad97c3a33b1c6
Steven Roland <steven@mojave.io>
2017-11-04 21:26:31 -0400

    Completed project

--- server.js
+++ server.js
@@ -1,39 +1,97 @@
-// server.js
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');
 var bodyParser = require('body-parser');
+var app        = express();
+var morgan     = require('morgan');
+
+app.use(morgan('dev')); // log requests to the console
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port     = process.env.PORT || 8080; // set our port
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/scotch_express_node_api'); // connect to our database
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+db.once('open', function() {
+  console.log("DB connection alive");
 });
 
-// more routes for our API will happen here
+var Bear     = require('./app/models/bear');
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+var router = express.Router();
+
+router.use(function(req, res, next) {
+	console.log('Something is happening.');
+	next();
+});
+
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+router.route('/bears')
+	.post(function(req, res) {
+		
+		var bear = new Bear();
+		bear.name = req.body.name; 
+
+		bear.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Bear created!' });
+		});
+
+		
+	})
+	.get(function(req, res) {
+		Bear.find(function(err, bears) {
+			if (err)
+				res.send(err);
+
+			res.json(bears);
+		});
+	});
+
+router.route('/bears/:bear_id')
+	.get(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+			if (err)
+				res.send(err);
+			res.json(bear);
+		});
+	})
+	.put(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+
+			if (err)
+				res.send(err);
+
+			bear.name = req.body.name;
+			bear.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Bear updated!' });
+			});
+
+		});
+	})
+	.delete(function(req, res) {
+		Bear.remove({
+			_id: req.params.bear_id
+		}, function(err, bear) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
 app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
